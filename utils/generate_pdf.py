6d27b545bc3c87e220262f20d891f8eca023dfcd
alexraskin <alexraskin@fastmail.fm>
2022-01-17 15:16:20 -0700

    new utils

---
+++ utils/generate_pdf.py
@@ -0,0 +1,25 @@
+import os
+
+from fpdf import FPDF
+
+
+class PdfReport:
+    def __init__(self, filename: str, guesses: list) -> None:
+        self.filename = filename
+        self.guesses = guesses
+
+    def generate(self):
+        pdf = FPDF(orientation='P', unit='pt', format='A4')
+        pdf.add_page()
+        pdf.set_font(family='Times', size=25, style='B')
+        pdf.cell(w=0, h=80, txt="LhGuess Report", border=0, align="C", ln=1)
+        pdf.image("files/lhcloudy.jpeg", w=100, h=100)
+
+
+        pdf.set_font(family="Times", size=12)
+        for guess in self.guesses:
+            pdf.multi_cell(w=100, h=20, txt=guess, border=0, align="C")
+
+
+        os.chdir("files")
+        pdf.output(self.filename)
---
+++ utils/uploader.py
@@ -0,0 +1,22 @@
+import json
+import os
+import sys
+
+from filestack import Client
+
+if not os.path.isfile("config.json"):
+    sys.exit("'config.json' not found! Please add it and try again.")
+else:
+    with open("config.json") as file:
+        config = json.load(file)
+
+
+class FileSharer:
+    def __init__(self, filepath, api_key=config["filestack_key"]):
+        self.filepath = filepath
+        self.api_key = api_key
+
+    def share(self):
+        client = Client(self.api_key)
+        new_filelink = client.upload(filepath=self.filepath)
+        return new_filelink.url
