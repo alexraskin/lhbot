6b2a0fc9737cf2a8a489bec2dd504813fd56aeb9
alexraskin <alexraskin@fastmail.fm>
2022-01-17 10:33:58 -0700

    fix imports

--- database/db.py
+++ database/db.py
@@ -1,13 +1,13 @@
-import os
 import json
+import os
 import sys
+
+import motor.motor_asyncio
 
 if not os.path.isfile("config.json"):
     sys.exit("'config.json' not found! Please add it and try again.")
 else:
     with open("./config.json", encoding="utf-8") as file:
         config = json.load(file)
-
-import motor.motor_asyncio
 
 client = motor.motor_asyncio.AsyncIOMotorClient(config["DATABASE_URL"])
