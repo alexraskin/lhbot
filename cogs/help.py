0c02bb342820aec3a87d667505bde943fc0c51a1
alexraskin <alexraskin@fastmail.fm>
2022-01-16 20:40:24 -0700

    add encoding

--- cogs/help.py
+++ cogs/help.py
@@ -8,7 +8,7 @@ from discord.ext import commands
 if not os.path.isfile("config.json"):
     sys.exit("'config.json' not found! Please add it and try again.")
 else:
-    with open("config.json") as file:
+    with open("config.json", encoding='utf-8') as file:
         config = json.load(file)
 
 
