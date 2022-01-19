a9df40a73e0435d9c4172bb0d74fa396f0ad1764
alexraskin <alexraskin@fastmail.fm>
2022-01-19 10:08:03 -0700

    add more lhcloudy commands

--- cogs/lhcloudy.py
+++ cogs/lhcloudy.py
@@ -1,6 +1,7 @@
 import json
 import os
 import sys
+import random
 
 import discord
 from discord.ext import commands
@@ -20,77 +21,93 @@ class LhCloudy(commands.Cog, name="lhcloudy"):
     def __init__(self, bot):
         self.bot = bot
 
+    @staticmethod
+    def random_emoji():
+        emoji_list = [
+            "😁",
+            "🥶",
+            "👌",
+            "👀",
+            "🤖",
+            "👽",
+            "💀",
+            "🤯",
+            "🤠",
+            "📼",
+            "📈",
+            "🧡",
+            "✨"
+        ]
+        return random.choice(list(emoji_list))
+
     @commands.command(name="1frame")
     async def one_frame(self, ctx):
-        url = 'https://www.twitch.tv/lhcloudy27/clip/AbstruseKindRuffFutureMan-dLWae-FGvNag2jtK'
-        embed = discord.Embed(color=0x42F56C)
-        embed.add_field(
-                    name="1Frame", value=url, inline=True
-                )
-        embed_message = await ctx.send(embed=embed)
-        await embed_message.add_reaction("📼")
-    
-    @commands.command(name='360')
+        url = "https://www.twitch.tv/lhcloudy27/clip/AbstruseKindRuffFutureMan-dLWae-FGvNag2jtK"
+        message = await ctx.send(url)
+        await message.add_reaction(LhCloudy.random_emoji())
+
+    @commands.command(name="360")
     async def three_sixty(self, ctx):
         url = "https://clips.twitch.tv/GentleObservantJalapenoLeeroyJenkins-Z53XsWoa2wamtAlB"
-        embed = discord.Embed(color=0x42F56C)
-        embed.add_field(
-                    name="360", value=url, inline=True
-                )
-        embed_message = await ctx.send(embed=embed)
-        await embed_message.add_reaction("📼")
-    
+        message = await ctx.send(url)
+        await message.add_reaction(LhCloudy.random_emoji())
+
     @commands.command(name="ball")
     async def ball(self, ctx):
         url = "https://gyazo.com/7f7dc8b93c4a77054104ee3d2ed9a134"
-        embed = discord.Embed(color=0x42F56C)
-        embed.add_field(
-                    name="Ball", value=url, inline=True
-                )
-        embed_message = await ctx.send(embed=embed)
-        await embed_message.add_reaction("📼")
+        message = await ctx.send(url)
+        await message.add_reaction(LhCloudy.random_emoji())
 
     @commands.command(name="bhop")
     async def bhop(self, ctx):
         url = "https://www.twitch.tv/lhcloudy27/clip/ToughAttractiveHerbsBlargNaut-GvSHZicwk7Sjs13X"
-        embed = discord.Embed(color=0x42F56C)
-        embed.add_field(
-                    name="bhop", value=url, inline=True
-                )
-        embed_message = await ctx.send(embed=embed)
-        await embed_message.add_reaction("📼")
+        message = await ctx.send(url)
+        await message.add_reaction(LhCloudy.random_emoji())
 
     @commands.command(name="bhop2")
     async def bhop_two(self, ctx):
         url = "https://clips.twitch.tv/BovineAverageChimpanzeeOpieOP-x5nayUCnonkU2OKc"
-        embed = discord.Embed(color=0x42F56C)
-        embed.add_field(
-                    name="bhop2", value=url, inline=True
-                )
-        embed_message = await ctx.send(embed=embed)
-        await embed_message.add_reaction("📼")
-    
+        message = await ctx.send(url)
+        await message.add_reaction(LhCloudy.random_emoji())
+
     @commands.command(name="climb")
     async def climb(self, ctx):
-        text = "Hey Cloudy. I am a diamond level tank player with aspirations to climb higher.\
-            I watch your streams every day in order to learn and get better.\
-            Now after studying your gameplay and applying it I have tanked to bronze. Thanks and much love."
-        embed = discord.Embed(color=0x42F56C)
-        embed.add_field(
-                    name="climb", value=text, inline=True
-                )
-        embed_message = await ctx.send(embed=embed)
-        await embed_message.add_reaction("📼")
-    
+        text = """Hey Cloudy. I am a diamond level tank player with aspirations to climb higher. I watch your streams every day in order to learn and get better. Now after studying your gameplay and applying it I have tanked to bronze. Thanks and much love."""
+        message = await ctx.send(text)
+        await message.add_reaction(LhCloudy.random_emoji())
+
     @commands.command(name="code")
     async def code(self, ctx):
         text = "rein: XEEAE | other: https://workshop.codes/u/Seita%232315"
-        embed = discord.Embed(color=0x42F56C)
-        embed.add_field(
-                    name="code", value=text, inline=True
-                )
-        embed_message = await ctx.send(embed=embed)
-        await embed_message.add_reaction("📼")
+        message = await ctx.send(text)
+        await message.add_reaction(LhCloudy.random_emoji())
+
+    @commands.command(name="dva")
+    async def dva(self, ctx):
+        url = "https://www.twitch.tv/lhcloudy27/clip/SmokyDifferentCobblerDoggo-rY6mWkS8b2Jj5kDm"
+        message = await ctx.send(url)
+        await message.add_reaction(LhCloudy.random_emoji())
+    
+    @commands.command(name="dva2")
+    async def dva(self, ctx):
+        url = "	https://clips.twitch.tv/WanderingLuckyClipzBrainSlug-0x2XxJjniDP_SSeX"
+        message = await ctx.send(url)
+        await message.add_reaction(LhCloudy.random_emoji())
+    
+    @commands.command(name="dva2")
+    async def dva(self, ctx):
+        url = "	https://clips.twitch.tv/ThirstySavageMinkAsianGlow-drRcT2-cwpRSx2gE"
+        message = await ctx.send(url)
+        await message.add_reaction(LhCloudy.random_emoji())
+
+    @commands.command(name="egirl")
+    async def dva(self, ctx):
+        text = "hey big cwoudy man, cawn i pwease be uw egiww mewcy?"
+        message = await ctx.send(text)
+        await message.add_reaction("👉")
+        await message.add_reaction("👈")
+        await message.add_reaction("😌")
+
 
 
 def setup(bot):
