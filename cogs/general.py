7dba2410775d87b81edaa21949dfdc75507666c6
alexraskin <alexraskin@fastmail.fm>
2022-01-21 22:54:07 -0700

    fix github cog url

--- cogs/general.py
+++ cogs/general.py
@@ -165,7 +165,7 @@ class General(commands.Cog, name="general"):
         module = cmd.module
         saucelines, startline = getsourcelines(cmd.callback)
         url = (
-            '<https://github.com/alexraskin/lbhot/blob/main/'
+            '<https://github.com/alexraskin/lhhot/blob/main/'
             f'{"/".join(module.split("."))}.py#L{startline}>\n'
         )
         sauce = ''.join(saucelines)
