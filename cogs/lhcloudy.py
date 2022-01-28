293cdc198b2b1185a2fcaa3ffe98c4bdf5648471
alexraskin <alexraskin@fastmail.fm>
2022-01-28 16:59:07 -0700

    break fix?

--- cogs/lhcloudy.py
+++ cogs/lhcloudy.py
@@ -3,8 +3,7 @@ import datetime
 from discord import Embed
 from discord.ext import commands
 
-
-from ..utils.emojis import random_emoji
+from utils.emojis import random_emoji
 
 
 class LhCloudy(commands.Cog, name="lhcloudy"):
