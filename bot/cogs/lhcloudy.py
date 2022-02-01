0d41c938943136b9b45ab87e517d11f13c3c8e67
alexraskin <alexraskin@fastmail.fm>
2022-01-31 20:58:52 -0700

    fix imports

--- bot/cogs/lhcloudy.py
+++ bot/cogs/lhcloudy.py
@@ -3,7 +3,7 @@ import datetime
 from discord import Embed
 from discord.ext import commands
 
-from bot.utils.emojis import random_emoji
+from utils.emojis import random_emoji
 
 
 class LhCloudy(commands.Cog, name="lhcloudy"):
