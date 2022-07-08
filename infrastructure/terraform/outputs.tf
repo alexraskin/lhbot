d0a170f9cef6fe5b63e806aa0c424f5eba48652d
alexraskin <alexraskin@fastmail.fm>
2022-07-08 15:51:38 -0700

    update output

--- infrastructure/terraform/outputs.tf
+++ infrastructure/terraform/outputs.tf
@@ -3,26 +3,7 @@ output "app_name" {
   description = "Application name"
 }
 
-output "app_region" {
-  value       = heroku_app.lhbot.region
-  description = "Application region"
-}
-
-output "app_stack" {
-  value       = heroku_app.lhbot.stack
-  description = "Application stack"
-}
-
-output "release_id" {
-  value       = heroku_app_release.lhbot.id
-  description = "Release ID"
-}
-
 output "status" {
   value       = heroku_build.lhbot.status
   description = "Application status"
-}
-
-output "cluster_connection_sting" {
-  value = split("//", mongodbatlas_cluster.lhcloudy_cluster.connection_strings.0.standard_srv)[1]
 }
