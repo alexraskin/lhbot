ee875ccfae5c1094c345ee7329ad75506da20438
Alex Raskin <alexraskin@fastmail.fm>
2022-11-22 21:50:04 -0700

    add some new commands

--- bot/cogs/general.py
+++ bot/cogs/general.py
@@ -1,13 +1,10 @@
 import platform
-import re
 from inspect import getsourcelines
-from urllib.parse import quote_plus
+import datetime, time
 
-from aiohttp import ContentTypeError
 from discord import DMChannel, Embed
 from discord.ext import commands
-from sentry_sdk import capture_exception
-from utils.bot_utils import get_year_round, get_year_string, progress_bar
+from utils.bot_utils import get_year_round, progress_bar
 
 
 class General(commands.Cog, name="General"):
@@ -21,8 +18,26 @@ class General(commands.Cog, name="General"):
         self.client = client
 
     @commands.Cog.listener()
+    async def on_ready(self):
+        global startTime
+        startTime = time.time()
+
+    @commands.Cog.listener()
     async def on_message(self, message):
-        await on_message_execute(message)
+        """
+        The on_message function specifically handles messages that are sent to the bot.
+        It checks if the message is a command, and then executes it if it is.
+
+        :param self: Used to access the class' attributes and methods.
+        :param message: Used to store information about the message.
+        :return: None.
+        """
+
+        if message.author.bot:
+            return
+
+        if isinstance(message.channel, DMChannel):
+            return
 
     @commands.cooldown(1, 15, commands.BucketType.user)
     @commands.command(name="info", aliases=["botinfo"])
@@ -55,8 +70,23 @@ class General(commands.Cog, name="General"):
 
     @commands.cooldown(1, 15, commands.BucketType.user)
     @commands.command(name="ping")
-    async def ping(self, ctx):
-        await ping_execute(ctx, round(self.client.latency * 1000))
+    async def ping_execute(self, ctx):
+        """
+        The ping function is used to check the bot's latency.
+        It returns a message with the time it takes for a message
+        to reach Discord and be received by the bot.
+
+        :param ctx: Used to get the context of where the command was called.
+        :param latency: Latency of the bot.
+        :return: a discord embed.
+        """
+        embed = Embed(
+            title="🏓 Pong!",
+            description=f"The bot latency is {round(self.client.latency * 1000)}ms.",
+            color=0x42F56C,
+        )
+        embed.set_footer(text=f"Requested by {ctx.message.author}")
+        await ctx.send(embed=embed)
 
     @commands.cooldown(1, 5, commands.BucketType.user)
     @commands.command(name="year", aliases=["yearprogress"])
@@ -78,69 +108,9 @@ class General(commands.Cog, name="General"):
         )
         await ctx.send(embed=embed)
 
-    @commands.cooldown(1, 10, commands.BucketType.user)
-    @commands.command(name="search", aliases=["lmgtfy", "duck", "duckduckgo", "google"])
-    async def search(self, ctx, query=None):
-        """
-        The search function is used to search for a query on duckduckgo.
-        It returns the first result of the query and sends it as an embed.
-
-        :param self: Used to access attributes and methods of the class.
-        :param ctx: Used to access the context of the command.
-        :param query=None: Used to make sure that the function cannot be called without a query.
-        :return: a discord embed object with the following format:.
-        """
-        if query is None:
-            await ctx.typing()
-            await ctx.send("Please enter a search query!")
-            return
-
-        if len(query) > 500:
-            await ctx.typing()
-            await ctx.send("Query size is too long!")
-            return
-
-        query = "+".join(query.split())
-        async with self.client.session.get(
-            f"https://api.duckduckgo.com/?format=json&t=lhbotdiscordbot&q={query}"
-        ) as response:
-            try:
-                answer = await response.json(content_type="application/x-javascript")
-            except ContentTypeError as e:
-                capture_exception(e)
-                await ctx.typing()
-                await ctx.send("Invalid query")
-                return
-
-            if (not answer) or (not answer["AbstractText"]):
-                await ctx.typing()
-                await ctx.send(
-                    "Couldn't find anything, here's duckduckgo link: "
-                    + f"<https://duckduckgo.com/?q={quote_plus(query)}>"
-                )
-                return
-
-            embed = Embed(description=answer["AbstractText"], color=0x2ECC71)
-
-            if answer["Image"]:
-                embed.set_image(url=f'https://api.duckduckgo.com{answer["Image"]}')
-
-            embed.set_author(
-                name=answer["Heading"],
-                icon_url="https://api.duckduckgo.com/favicon.ico",
-            )
-
-            embed.set_footer(
-                text=f'Info from {answer["AbstractSource"]}\n'
-                + f'at {answer["AbstractURL"]}\n'
-                + "Provided By: https://api.duckduckgo.com"
-            )
-            await ctx.typing()
-            await ctx.send(embed=embed)
-
     @commands.cooldown(1, 60, commands.BucketType.user)
     @commands.command(name="inspect", hidden=True)
-    async def inspect(self, ctx, *, command_name: str):
+    async def inspect(self, ctx, command_name: str):
         """
         The inspect function is used to print the source code of a command.
         It's useful for figuring out what exactly a command does, and how it works.
@@ -167,62 +137,22 @@ class General(commands.Cog, name="General"):
         await ctx.typing()
         await ctx.send(url + f"```python\n{sanitized}\n```")
 
-
-async def ping_execute(ctx, latency):
-    """
-    The ping function is used to check the bot's latency.
-    It returns a message with the time it takes for a message
-    to reach Discord and be received by the bot.
-
-    :param ctx: Used to get the context of where the command was called.
-    :param latency: Latency of the bot.
-    :return: a discord embed.
-    """
-    embed = Embed(
-        title="🏓 Pong!",
-        description=f"The bot latency is {latency}ms.",
-        color=0x42F56C,
+    @commands.command(
+        name="uptime", aliases=["up"], description="Shows the uptime of the bot"
     )
-    embed.set_footer(text=f"Requested by {ctx.message.author}")
-    await ctx.send(embed=embed)
+    async def uptime(self, ctx):
+        """
+        The uptime function is used to show the uptime of the bot.
+        """
+        await ctx.typing()
+        uptime = str(datetime.timedelta(seconds=int(round(time.time() - startTime))))
 
+        embed = Embed(
+            title="Bot Uptime", description=f"Uptime: {uptime}", color=0x42F56C
+        )
+        embed.set_footer(text=f"Requested by {ctx.message.author}")
 
-async def on_message_execute(message):
-    """
-    The on_message function specifically handles messages that are sent to the bot.
-    It checks if the message is a command, and then executes it if it is.
-
-    :param self: Used to access the class' attributes and methods.
-    :param message: Used to store information about the message.
-    :return: None.
-    """
-    if message.author.bot:
-        return
-
-    if isinstance(message.channel, DMChannel):
-        return
-
-    if re.search(r"(?i)^(?:hi|what\'s up|yo|hey|hello) lhbot", message.content):
-        await message.channel.send("hello")
-
-    if re.search(
-        r"(?i)(?:the|this) (?:current )?year is "
-        + r"(?:almost |basically )?(?:over|done|finished)",
-        message.content,
-    ):
-        await message.channel.send(get_year_string())
-
-    if re.search(r"(?i)^you wanna fight, lhbot\?", message.content):
-        await message.channel.send("bring it on pal (╯°□°）╯︵ ┻━┻")
-
-    if re.search(r"(?i)^lhbot meow", message.content):
-        await message.channel.send("ฅ^•ﻌ•^ฅ")
-
-    if re.search(
-        r"(?i)^lh what(?:\'s| is) the answer to life,? the universe and everything",
-        message.content,
-    ):
-        await message.channel.send("42")
+        await ctx.send(embed=embed)
 
 
 async def setup(client):
