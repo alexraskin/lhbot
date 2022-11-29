1f50f6076f6390b438013c620295b0cc489762c8
Alex Raskin <alexraskin@fastmail.fm>
2022-11-28 23:29:55 -0700

    fmt

--- bot/cogs/web_server.py
+++ bot/cogs/web_server.py
@@ -10,8 +10,10 @@ from aiohttp import web
 from discord.ext import commands
 from discord import __version__ as discord_version
 
+
 def get_uptime(start_time):
     return str(datetime.timedelta(seconds=int(round(time.time() - start_time))))
+
 
 class WebServer(commands.Cog, name="WebServer"):
     def __init__(self, client):
@@ -24,18 +26,15 @@ class WebServer(commands.Cog, name="WebServer"):
     @aiohttp_jinja2.template("index.html")
     async def index_handler(self, request):
         return {
-          "discord_version": discord_version,
-          "bot_version": self.client.version,
-          "bot_latency": f"{round(self.client.latency * 1000)}ms",
-          "bot_uptime": get_uptime(self.client.start_time),
-          }
+            "discord_version": discord_version,
+            "bot_version": self.client.version,
+            "bot_latency": f"{round(self.client.latency * 1000)}ms",
+            "bot_uptime": get_uptime(self.client.start_time),
+        }
 
     async def webserver(self):
-
         app = web.Application()
-        aiohttp_jinja2.setup(
-            app, loader=jinja2.FileSystemLoader(str(self.path))
-        )
+        aiohttp_jinja2.setup(app, loader=jinja2.FileSystemLoader(str(self.path)))
         app.router.add_get("/", self.index_handler)
         runner = web.AppRunner(app)
         await runner.setup()
