c409fe9d39c0792ad5402a82aab38ef2a9e26f2c
Alex Raskin <alexraskin@fastmail.fm>
2022-12-05 20:54:28 -0700

    fix 1v1

--- bot/cogs/onevone.py
+++ bot/cogs/onevone.py
@@ -6,6 +6,10 @@ from discord.ext import commands
 
 
 class OneVOne(commands.Cog, name="OneVOne"):
+    """
+    One V One another user in the server!
+    """
+
     def __init__(self, client: commands.Bot):
 
         self.client = client
@@ -53,12 +57,14 @@ class OneVOne(commands.Cog, name="OneVOne"):
 
     @commands.command(
         name="1v1",
-        description="Random Hero 1v1",
+        description="Random Overwatch Hero 1v1",
     )
     async def one_v_one(
-        self, ctx: commands.Context, user: Member,
+        self,
+        ctx: commands.Context,
+        user: Member = "",
     ):
-        if user is None:
+        if user is None or "":
             await ctx.send("Please target another user to 1v1")
             return
 
@@ -70,8 +76,13 @@ class OneVOne(commands.Cog, name="OneVOne"):
 
         embed = Embed(title="Overwatch Random Hero 1v1", color=0x00FF00)
 
-        embed.add_field(name=f"{str(ctx.author).strip('#')} is playing", value=f"{hero_one} with {hero_one_health} health")
-        embed.add_field(name=f"{user} is playing", value=f"{hero_two} with {hero_two_health} health")
+        embed.add_field(
+            name=f"{str(ctx.author).strip('#')} is playing",
+            value=f"{hero_one} with {hero_one_health} health",
+        )
+        embed.add_field(
+            name=f"{user} is playing", value=f"{hero_two} with {hero_two_health} health"
+        )
 
         first_message = await ctx.send(embed=embed)
 
@@ -80,20 +91,26 @@ class OneVOne(commands.Cog, name="OneVOne"):
             hero_one_health -= random.randint(0, hero_one_health)
             hero_two_health -= random.randint(0, hero_two_health)
             await asyncio.sleep(1)
-            second_embed.add_field(name=f"{ctx.author}", value=f"Playing {hero_one} with {hero_one_health} health")
-            second_embed.add_field(name=f"{user}", value=f"Playing {hero_two} with {hero_two_health} health")
+            second_embed.add_field(
+                name=f"{ctx.author}",
+                value=f"Playing {hero_one} with {hero_one_health} health",
+            )
+            second_embed.add_field(
+                name=f"{user}",
+                value=f"Playing {hero_two} with {hero_two_health} health",
+            )
             final = await first_message.edit(embed=second_embed)
             win_embed = Embed(title="Overwatch Random Hero 1v1", color=0x00FF00)
-        if hero_one_health == 0:
-          win_embed.add_field(name=ctx.author, value=f"Won, playing {hero_one}!")
-          win_embed.add_field(name=user, value=f"Lost, playing {hero_two}")
-        elif hero_two_health == 0:
-          win_embed.add_field(name=ctx.author, value=f"Lost, playing {hero_one}")
-          win_embed.add_field(name=user, value=f"Won, playing {hero_two}")
-        
-        await final.edit(embed=win_embed)
-              
 
+        if hero_one_health == 0:
+            win_embed.add_field(name=ctx.author, value=f"Won, playing {hero_one}!")
+            win_embed.add_field(name=user, value=f"Lost, playing {hero_two}")
+
+        elif hero_two_health == 0:
+            win_embed.add_field(name=ctx.author, value=f"Lost, playing {hero_one}")
+            win_embed.add_field(name=user, value=f"Won, playing {hero_two}")
+
+        await final.edit(embed=win_embed)
 
     def get_random_hero(self) -> str:
         all = self.tank_heros | self.dmg_heros | self.support_heros
