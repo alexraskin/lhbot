049e821801e4394151344530115e4764efe2cf7d
alexraskin <alexraskin@fastmail.fm>
2023-07-30 14:44:49 -0700

    release fix

--- release.py
+++ release.py
@@ -15,7 +15,6 @@ class CreateRelease:
         self.latest_release_url = "https://api.github.com/repos/alexraskin/lhbot/tags"
         self.url = f"https://api.github.com/repos/alexraskin/lhbot/releases"
 
-
     def create_release(self, version, description):
         self.headers = {
             "Accept": "application/vnd.github+json",
@@ -34,22 +33,21 @@ class CreateRelease:
         }
         response = requests.post(self.url, headers=self.headers, json=self.data)
         return json.dumps(response.json(), indent=4, sort_keys=True)
-    
+
     def get_release(self):
         response = requests.get(self.latest_release_url)
         return response.json()
 
 
 if __name__ == "__main__":
-  create_release = CreateRelease()
+    create_release = CreateRelease()
 
-  print(f"Latest release: ", create_release.get_release()[0]['name'])
+    print(f"Latest release: ", latest_release := create_release.get_release()[0]["name"])
+    print(f"New version number:", version_number := f"{latest_release.split('.')[0]}.{int(latest_release.split('.')[1]) + 1}")
 
-  argparse = argparse.ArgumentParser()
-  argparse.add_argument("--version", help="Version number")
-  argparse.add_argument("--description", help="Description of the release")
-  args = argparse.parse_args()
-  version_number = args.version
-  description = args.description
+    argparse = argparse.ArgumentParser()
+    argparse.add_argument("--description", help="Description of the release")
+    args = argparse.parse_args()
+    description = args.description
 
-  print(create_release.create_release(version_number, description))
+    print(create_release.create_release(version_number, description))
