6df1e82a86ce094e25164ac52d075f5bce17cdc6
alexraskin <alexraskin@fastmail.fm>
2024-05-21 23:14:12 -0700

    🔄 Refactor cog commands in lhcloudy.py.

--- bot/cogs/lhcloudy.py
+++ bot/cogs/lhcloudy.py
@@ -1,9 +1,8 @@
 from __future__ import annotations
 
 import datetime
-import sys
 
-from typing import Optional, TYPE_CHECKING
+from typing import TYPE_CHECKING
 
 from discord import Colour, Embed
 from discord.ext import commands
@@ -16,28 +15,21 @@ class LhCloudy(commands.Cog):
     def __init__(self, client: LhBot):
         self.client: LhBot = client
 
-    @commands.hybrid_group(invoke_without_command=True)
-    async def twitch(self, ctx: commands.Context):
-        if ctx.invoked_subcommand is None:
-            await ctx.send(
-                f"Please use `{self.client.config.bot_prefix}twitch help` to see the list of commands."
-            )
-
-    @twitch.command(name="lhfurry", with_app_command=True)
+    @commands.hybrid_command(name="lhfurry", with_app_command=True)
     async def lhfurry(self, ctx: commands.Context) -> None:
         """
         LhCloudy's furry side
         """
         await ctx.send("https://i.gyazo.com/3ae8376713000ab829a2853d0f31e6f2.png")
 
-    @twitch.command(name="code", aliases=["workshop"], with_app_command=True)
+    @commands.hybrid_command(name="code", aliases=["workshop"], with_app_command=True)
     async def code(self, ctx: commands.Context) -> None:
         """
         LhCloudy's workshop code
         """
         await ctx.send("rein: XEEAE | other: https://workshop.codes/u/Seita%232315")
 
-    @twitch.command(name="egirl", with_app_command=True)
+    @commands.hybrid_command(name="egirl", with_app_command=True)
     async def egirl(self, ctx: commands.Context):
         """
         Egirl Quote
@@ -48,14 +40,14 @@ class LhCloudy(commands.Cog):
         await message.add_reaction("👈")
         await message.add_reaction("😌")
 
-    @twitch.command(name="instagram", with_app_command=True)
+    @commands.hybrid_command(name="instagram", with_app_command=True)
     async def instagram(self, ctx: commands.Context):
         """
         Get a link to LhCloudy's Instagram
         """
         await ctx.send("https://www.instagram.com/lhcloudy/")
 
-    @twitch.command(name="spotify", with_app_command=True)
+    @commands.hybrid_command(name="spotify", with_app_command=True)
     async def spotify(self, ctx: commands.Context):
         """
         Get a link to LhCloudy's Spotify Playlist
@@ -64,7 +56,7 @@ class LhCloudy(commands.Cog):
             "https://open.spotify.com/playlist/3JuA2BZjl0aZsEHKry1B67?si=14278d6ea4c04330"
         )
 
-    @twitch.command(name="playlist", with_app_command=True)
+    @commands.hybrid_command(name="playlist", with_app_command=True)
     async def playlist(self, ctx: commands.Context):
         """
         Get a link to LhCloudy's Youtube Music Playlist
@@ -73,21 +65,21 @@ class LhCloudy(commands.Cog):
             "https://www.youtube.com/watch?v=p1SlBlB5pzU&list=RDHiu1hPdJk-Y&index=23"
         )
 
-    @twitch.command(name="tips", with_app_command=True)
+    @commands.hybrid_command(name="tips", with_app_command=True)
     async def tips(self, ctx: commands.Context):
         """
         Get a link to LhCloudy's Rein Tips
         """
         await ctx.send("W+M1")
 
-    @twitch.command(name="twitter", with_app_command=True)
+    @commands.hybrid_command(name="twitter", with_app_command=True)
     async def twitter(self, ctx: commands.Context):
         """
         Get a link to LhCloudy's Twitter
         """
         await ctx.send("https://twitter.com/LhCloudy")
 
-    @twitch.command(name="youtube", with_app_command=True)
+    @commands.hybrid_command(name="youtube", with_app_command=True)
     async def youtube(self, ctx: commands.Context):
         """
         Get a link to LhCloudy's Youtube Channel
@@ -95,7 +87,7 @@ class LhCloudy(commands.Cog):
         url = "SMÄSH THAT LIKE AND SUBSCRIBE BUTTON -> https://www.youtube.com/channel/UC2CV-HWvIrMO4mUnYtNS-7A"
         await ctx.send(url)
 
-    @twitch.command(name="age", aliases=["oldman"], with_app_command=True)
+    @commands.hybrid_command(name="age", aliases=["oldman"], with_app_command=True)
     async def age(self, ctx: commands.Context):
         """
         Get LhCloudy's age
@@ -105,14 +97,37 @@ class LhCloudy(commands.Cog):
         age_years = int((td - bd).days / 365.25)
         await ctx.send(age_years)
 
-    @twitch.command(name="from", with_app_command=True)
+    @commands.hybrid_command(name="birthday", with_app_command=True)
+    async def birthday(self, ctx: commands.Context):
+        """
+        Get days until next birthday
+        """
+        birthday_str = "21.5.1999"
+        birthday = datetime.datetime.strptime(birthday_str, "%d.%m.%Y").date()
+        
+        today = datetime.date.today()
+        current_year_birthday = birthday.replace(year=today.year)
+        next_birthday = current_year_birthday
+        
+        if today > current_year_birthday:
+            next_birthday = birthday.replace(year=today.year + 1)
+        
+        days_until_next_birthday = (next_birthday - today).days
+
+        if days_until_next_birthday == 0:
+            await ctx.send("Today is Cloudy's birthday! 🎉")
+            await ctx.send("https://tenor.com/bmYbD.gif")
+        else:
+            await ctx.send(f"There are {days_until_next_birthday} days until Cloudy's birthday.")
+
+    @commands.hybrid_command(name="from", with_app_command=True)
     async def from_(self, ctx: commands.Context):
         """
         Where is LhCloudy from?
         """
         await ctx.send("kotka of south eastern finland of the continent of europe")
 
-    @twitch.command(name="links", aliases=["urls"], with_app_command=True)
+    @commands.hybrid_command(name="links", aliases=["urls"], with_app_command=True)
     async def links(self, ctx: commands.Context) -> None:
         """
         Get a list of LhCloudy's links
